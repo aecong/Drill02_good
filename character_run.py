a48949e75bf6b4430bde98f4f1f06dd90c1ae247
aecong <dkdus78655@gmail.com>
2023-09-20 18:13:31 +0900

    화면 그리기를 함수화 시킴
    character, grass

--- character_run.py
+++ character_run.py
@@ -6,6 +6,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def rander_all(x ,y):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)
+        
 def run_circle():
     print("circle")
 
@@ -13,25 +19,22 @@ def run_circle():
     for deg in range(0,360,5):
         x = cx + r * math.cos(deg/360 * 2 * math.pi)
         y = cy + r * math.sin(deg/360 * 2 * math.pi)
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
+        rander_all(x, y)
 
 def run_rectangle():
     print("rectangle")
 
     #bottom line
     for x in range (50, 750+1, 10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
-      
+         rander_all(x, 90)
+    #top line
+     #for x in range (750, 50, -10):
+        
+   
     pass
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()      
     break
 

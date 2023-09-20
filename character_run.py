18ed35d70516db7139da02b1acd4300fa634f9a4
aecong <dkdus78655@gmail.com>
2023-09-20 17:39:17 +0900

    전체 축을 형성
    함수를 미리 만든다

--- character_run.py
+++ character_run.py
@@ -1,5 +1,12 @@
+import math
 from pico 2d import *
 
 open_canvas()
 
+grass = load_image('grass.png')
+charactoer = load_image('character.png')
+
+while True:
+    run_circle()
+    run_rectangle()
 close_canvas()

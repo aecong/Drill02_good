ead0fee8c70b29cb6af0e43f6c5ee969b8e1a138
aecong <dkdus78655@gmail.com>
2023-09-20 17:46:09 +0900

    화면에 grass, character 그리기

--- character_run.py
+++ character_run.py
@@ -4,10 +4,14 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-charactoer = load_image('character.png')
+character = load_image('character.png')
 
 def run_circle():
     print("circle")
+    clear.canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
     pass
 def run_rectangle():
     print("rectangle")
@@ -16,5 +20,5 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
-
+    break
 close_canvas()

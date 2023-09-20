17744c8374afe4a943aa247574122dab5c78bd5d
aecong <dkdus78655@gmail.com>
2023-09-20 17:42:00 +0900

    print로 함수가 잘 작동하는지 확인

--- character_run.py
+++ character_run.py
@@ -6,7 +6,16 @@ open_canvas()
 grass = load_image('grass.png')
 charactoer = load_image('character.png')
 
+def run_circle():
+    print("circle")
+    pass
+def run_rectangle():
+    print("rectangle")
+    pass
+
 while True:
     run_circle()
     run_rectangle()
+    break
+
 close_canvas()

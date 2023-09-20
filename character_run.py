6f3fe1e31d8e7404b8b4c3eab8803e9e01eb8b53
aecong <dkdus78655@gmail.com>
2023-09-20 18:27:51 +0900

    사각형 연결되게 이동 완성!

--- character_run.py
+++ character_run.py
@@ -25,21 +25,20 @@ def run_rectangle():
     print("rectangle")
 
     #bottom line
-    #for x in range (50, 750+1, 10):
-    #    rander_all(x, 90)
-     
-    #top line
-    #for x in range (750, 50-1, -10):
-    #     rander_all(x,550)
-
-    #left line
-    #for y in range (550, 90-1, -10):
-    #    rander_all(50,y)
-
+    for x in range (50, 750+1, 10):
+        rander_all(x, 90)
+        
     #right line
     for y in range(90, 550 -1, 10):
         rander_all(750,y)
-    pass
+        
+    #top line
+    for x in range (750, 50-1, -10):
+         rander_all(x,550)
+
+    #left line
+    for y in range (550, 90-1, -10):
+        rander_all(50,y)  
 
 while True:
     #run_circle()
